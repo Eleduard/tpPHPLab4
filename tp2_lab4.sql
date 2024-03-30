438c52a36e042a25934d725b274395579fe59b7e
Alvaro <alvaro.godoy@alumnos.frm.utn.edu.ar>
2024-03-30 11:37:48 -0300

    Base de datos agregada

---
+++ tp2_lab4.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v8.55 
+MySQL - 8.0.34 : Database - tp2_lab4
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tp2_lab4` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `tp2_lab4`;
+
+/*Table structure for table `pais` */
+
+DROP TABLE IF EXISTS `pais`;
+
+CREATE TABLE `pais` (
+  `codigoPais` int NOT NULL,
+  `nombrePais` varchar(50) NOT NULL,
+  `capitalPais` varchar(50) NOT NULL,
+  `region` varchar(50) NOT NULL,
+  `poblacion` int NOT NULL,
+  `latitud` decimal(10,0) NOT NULL,
+  `longitud` decimal(10,0) NOT NULL,
+  PRIMARY KEY (`codigoPais`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+/*Data for the table `pais` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
